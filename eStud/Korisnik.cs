e6b84606a672eb27d48b314c88c409b18dfbdb54
melida0705 <44578817+melida0705@users.noreply.github.com>
2018-12-05 21:51:44 +0100

    Napravljen DB controller
    Napravljena forma za studenta i odradjen zahtev  Licni podaci

---
+++ eStud/Korisnik.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStud.Model
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class Korisnik 
+    {
+        private string username;
+        private string usertype;
+        private string ime;
+        private string prezime;
+        private string datumRodjenja;
+        private string pol;
+        public Korisnik(string username,string usertype,string ime,string prezime,string datumRodjenja,string pol)
+        {
+            this.username = username;
+            this.usertype = usertype;
+            this.ime = ime;
+            this.prezime = prezime;
+            this.datumRodjenja = datumRodjenja;
+            this.pol = pol;
+
+
+        }
+        public void setUserType(String usertype)
+        {
+            this.usertype = usertype;
+          
+        }
+        public void setUserName(String username)
+        {
+            this.username = username;
+            
+        }
+        public void setIme(String ime)
+        {
+            this.ime = ime;
+           
+        }
+        public void setPrezime(String prezime)
+        {
+            this.prezime = prezime;
+        
+        }
+        public void setDatumRodjenja(String datumRodjenja)
+        {
+            this.datumRodjenja = datumRodjenja;
+     
+        }
+        public void setPol(String pol)
+        {
+            this.pol = pol;
+            
+        }
+        public String getUserType()
+        {
+            return usertype;
+        }
+        public String getUserName()
+        {
+            return username;
+        }
+        public String getIme()
+        {
+            return ime;
+        }
+        public String getPrezime()
+        {
+            return prezime;
+        }
+        public String getDatumRodjenja()
+        {
+            return datumRodjenja;
+        }
+        public String getPol()
+        {
+            return pol;
+        }
+
+
+
+    }
+}
---
+++ eStud/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using eStud.Model;
+
+namespace eStud
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model.DBController dbc = new Model.DBController();
+        Model.Korisnik kor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Korisnik Korisnik { get => Korisnik; set => Korisnik = value; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Korisnik rezultatUpita = dbc.ImaUBazi(txtUser.Text,txtPass.Text);
+            
+             
+            if (rezultatUpita.getUserType() == "student")
+
+            {
+                try
+                {
+                    this.Hide();
+                    StudentWindow sf = new StudentWindow(rezultatUpita);
+                    sf.ShowDialog();
+                }
+                catch (Exception es) { MessageBox.Show(es.Message); }
+            }
+
+        }
+
+        private Student korisnik(string v1, object v2, object v3, object v4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
